d5dfe6af1b44437c72c206295547de60c3165a50
bk1607 <baluanumala16@gmail.com>
2023-12-28 10:18:02 +0530

    rabbitmq

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_spot_instance_request" "rabbitmq" {
   ami           = data.aws_ami.ami_id.id
   instance_type = "t3.micro"
-#  spot_type = "persistent"
-#  instance_interruption_behavior = "stop"
+  spot_type = "persistent"
+  instance_interruption_behavior = "stop"
 #  wait_for_fulfillment = true
   #availability_zone = "us-east-1"
   #subnet_id = var.sub_ids[0]
